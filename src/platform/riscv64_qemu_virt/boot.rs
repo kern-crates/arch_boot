b14c8eb94304da901ec47ef707215608937bf490
Shi Lei <shi_lei@massclouds.com>
2024-05-12 02:55:50 +0000

    early_mmu: merge it into boot to keep consistent with other arch

--- src/platform/riscv64_qemu_virt/boot.rs
+++ src/platform/riscv64_qemu_virt/boot.rs
@@ -1,5 +1,11 @@
+use riscv::register::satp;
 use axconfig::{PHYS_VIRT_OFFSET, TASK_STACK_SIZE};
-use early_mmu::platform::{BOOT_STACK, init_boot_page_table, init_mmu};
+
+#[link_section = ".bss.stack"]
+pub static mut BOOT_STACK: [u8; TASK_STACK_SIZE] = [0; TASK_STACK_SIZE];
+
+#[link_section = ".data.boot_page_table"]
+static mut BOOT_PT_SV39: [u64; 512] = [0; 512];
 
 /// The earliest entry point for the primary CPU.
 #[naked]
@@ -36,4 +42,17 @@ unsafe extern "C" fn _start() -> ! {
         entry = sym super::rust_entry,
         options(noreturn),
     )
+}
+
+unsafe fn init_boot_page_table() {
+    // 0x8000_0000..0xc000_0000, VRWX_GAD, 1G block
+    BOOT_PT_SV39[2] = (0x80000 << 10) | 0xef;
+    // 0xffff_ffc0_8000_0000..0xffff_ffc0_c000_0000, VRWX_GAD, 1G block
+    BOOT_PT_SV39[0x102] = (0x80000 << 10) | 0xef;
+}
+
+unsafe fn init_mmu() {
+    let page_table_root = BOOT_PT_SV39.as_ptr() as usize;
+    satp::set(satp::Mode::Sv39, 0, page_table_root >> 12);
+    riscv::asm::sfence_vma_all();
 }
