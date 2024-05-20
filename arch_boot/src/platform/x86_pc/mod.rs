56361b18af85fa0a57a9a2664dd9635089d2636c
Shi Lei <shi_lei@massclouds.com>
2024-05-20 02:13:11 +0000

    fix x86_64 parameters to runtime_main

--- arch_boot/src/platform/x86_pc/mod.rs
+++ arch_boot/src/platform/x86_pc/mod.rs
@@ -4,9 +4,16 @@ unsafe extern "C" fn rust_entry(magic: usize, mbi: usize) {
     // TODO: handle multiboot info
     if magic == self::boot::MULTIBOOT_BOOTLOADER_MAGIC {
         super::clear_bss();
-        runtime_main(magic, mbi);
+        runtime_main(current_cpu_id(), mbi);
     }
     unreachable!();
+}
+
+fn current_cpu_id() -> usize {
+    match raw_cpuid::CpuId::new().get_feature_info() {
+        Some(finfo) => finfo.initial_local_apic_id() as usize,
+        None => 0,
+    }
 }
 
 extern "Rust" {
